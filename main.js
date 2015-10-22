c19c4a1349ef5946746f7fd5588f3df9eb614fcf
Mychelle Blake <mychelle.blake@gmail.com>
2015-10-22 16:39:15 -0700

    Adds revised files

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
 
-	var template = $(".allOtherCategoriesTemplate").text();
+	var templateString = $("#listItemTemplate").text();
 	var listHTML = Mustache.render(templateString,data);
-	$("#itemContainer").html(listHTML);
+	$("#pictureContainer").html(listHTML);
 
+//	var mapData = (data.results.map(function(obj)) {
+//		return {
+//			title: obj.title,
+//			price: obj.price,
+//			currency_code: obj.currency_code,
+//			url_170x135: obj.url_170x135[0]
+//		}
 });
+
+//newArr.forEach(function(obj) {
+//	$("#span2").append("<p>" + obj.title + ", $" + obj.price + "</p>");
+//});
+
+
+
+
+
