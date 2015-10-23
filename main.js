1d1856987ea8d134cfeb4ed94e729f84646261ba
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-10-23 09:50:22 -0700

    starting to format template

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
 
-	
+	var mapData = data.results.map(function(obj){
+		return {
+			'title': obj.title,
+			'price': obj.price,
+			'shop_name': obj.Shop.shop_name,
+			'currency_code':obj.currency_code,
+			'url_170x135': obj.Images[0]['url_170x135']
+		};
+	});
+
+	var newData = {mapData:mapData};
 
 var templateString = $("#listItemTemplate").text();
-var listHtml = Mustache.render(templateString, data);
+var listHtml = Mustache.render(templateString, newData);
 $("#container").html(listHtml);
 
 });
