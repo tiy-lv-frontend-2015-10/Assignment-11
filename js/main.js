449ad9189e453fab390912020a659f3a4d39df69
Kristie White <insanelynocturnal@gmail.com>
2015-10-23 22:57:25 -0700

    10/23 update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
 
-
+var listings = data.results.map(function(listing){
+    return {
+        listingId: listing.listing_id,
+        categoryId: listing.category_id,
+        url_75x75: listing.Images[0].url_75x75
+    };
+});
+    
+var listingData = {
+    "listings": listings
+} 
+    
+var listingTemplate = $("#listingTemplate").text();
+var listingHTML = Mustache.render(listingTemplate, listingData);
+$("#allCategories").html(listingHTML);
 
 });//closes doc ready
