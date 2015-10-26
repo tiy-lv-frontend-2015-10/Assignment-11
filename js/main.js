a6bde672024e0a9f2986ffc8d582e37ad899313e
Kristie White <insanelynocturnal@gmail.com>
2015-10-25 23:22:15 -0700

    assignment-11

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+//this is to get CATEGORIES LIST info from data.js
 var listings = data.results.map(function(listing){
     return {
         listingId: listing.listing_id,
@@ -7,13 +8,39 @@ var listings = data.results.map(function(listing){
         url_75x75: listing.Images[0].url_75x75
     };
 });
-    
+
 var listingData = {
     "listings": listings
 } 
-    
+//send text, render to html, send data via jquery
 var listingTemplate = $("#listingTemplate").text();
 var listingHTML = Mustache.render(listingTemplate, listingData);
-$("#allCategories").html(listingHTML);
+$("#allCategoriesList").html(listingHTML); 
 
+
+    
+    
+//get TOP CATEGORIES
+//loop for categories that I cannot find
+    
+//$("#topCats").text();
+//Mustache.render( , );
+//send to html doc
+
+    
+    
+    
+//get ALL CATEGORIES WHISKEY -- loop ALL categories!
+    var allCategories = data.forEach(function(all){
+        return {
+            category_path: all.category_path,  
+        }
+    })
+    
+//send text, render to html, send data via jquery
+//$("#allCats").text();
+//Mustache.render( , );
+$("allCategoriesWhiskey").html()
+    
+    
 });//closes doc ready
