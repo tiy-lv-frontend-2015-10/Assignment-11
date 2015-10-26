868565b853669f22345b8cbea84f591cfd9823f5
Domingo Rosa <DomingoRosa@Domingos-MacBook-Air.local>
2015-10-26 16:07:00 -0700

    updated project

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-/*$(document).ready(function(){
+$(document).ready(function(){
 			
-var whiskeyPull = {
-  		"listPics": listPics
-		};
-	var whiskeyTemplate = $("#whiskeyTemplate").text();
-	var whiskeyHTML = Mustache.render(whiskeyTemplate, whiskeyPull);
-	$("#whiskeyProducts").html(whiskeyHTML);
-});
 
-	var listPics = results.map(function(item){
+
+
+	var whiskeyPics = data.results.map(function(item){
+		var string = item.title;
+		var length = 40;
+		var trimmedString = string.length > length ? 
+                    string.substring(0, length - 3) + "..." : 
+                    string.substring(0, length);
 		return {
-			url_75x75: item.Images[0].url_75x75,
-			title: item.title,
-			shop_name: item.shop_name,
-			price: item.price + item.currency_code
+			url_170x135: item.Images[0].url_170x135,
+			title: trimmedString,
+			shop_name: item.Shop.shop_name,
+			price: item.price + " " + item.currency_code
 		};
 	});
-	*/
+	var whiskeyPull = {
+	  		"whiskeyPics": whiskeyPics
+			};
+	var whiskeyTemplate = $("#whiskeyTemplate").text();
+	var whiskeyHTML = Mustache.render(whiskeyTemplate, whiskeyPull);
+		$("#whiskeyProducts").html(whiskeyHTML);
+});
