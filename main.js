3fde082f0659a663b01d3606c43cf2837a7451d9
notewrangler <noiseartist@gmail.com>
2015-10-25 17:13:37 -0700

    hover on pic panels done -- still no mustache functionality

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 $(document).ready(function(){
 
+
+	$('.product-square').hover(function(){
+			$(this).children().toggle();
+	});
+
+
+
+	// display the left side menu ***********************************
+
 	var categories = data.etsy_main_categories;
 	console.log(categories);
 
 	var categoryTemplate = $('#categoryTemplate').text();
 	console.log(categoryTemplate);
 
-	var categoryHTML = Mustache.render(categoryTemplate, categories);
+	var categoryData = categories;
+	console.log(categoryData);
+
+	var categoryHTML = Mustache.render(categoryTemplate, categoryData);
 	console.log(categoryHTML);
 	$("#top-side-list").html(categoryHTML);
-
+//*****************************************************************
 });
