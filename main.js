6910f9814305aaaede68ca3f4e930c13f82cdf04
Domingo Rosa <DomingoRosa@192.168.1.103>
2015-10-26 07:26:27 -0700

    not completed but turned in.

---
+++ main.js
@@ -0,0 +1,19 @@
+/*$(document).ready(function(){
+			
+var whiskeyPull = {
+  		"listPics": listPics
+		};
+	var whiskeyTemplate = $("#whiskeyTemplate").text();
+	var whiskeyHTML = Mustache.render(whiskeyTemplate, whiskeyPull);
+	$("#whiskeyProducts").html(whiskeyHTML);
+});
+
+	var listPics = results.map(function(item){
+		return {
+			url_75x75: item.Images[0].url_75x75,
+			title: item.title,
+			shop_name: item.shop_name,
+			price: item.price + item.currency_code
+		};
+	});
+	*/
