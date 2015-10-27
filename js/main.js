411657f5fe19ded8e02467de3a3dc7657e020393
Kristie White <insanelynocturnal@gmail.com>
2015-10-26 17:48:35 -0700

    mustache update

--- js/main.js
+++ js/main.js
@@ -1,46 +1,24 @@
 $(document).ready(function(){
 
-//this is to get CATEGORIES LIST info from data.js
-var listings = data.results.map(function(listing){
-    return {
-        listingId: listing.listing_id,
-        categoryId: listing.category_id,
-        url_75x75: listing.Images[0].url_75x75
-    };
-});
-
-var listingData = {
-    "listings": listings
-} 
-//send text, render to html, send data via jquery
-var listingTemplate = $("#listingTemplate").text();
-var listingHTML = Mustache.render(listingTemplate, listingData);
-$("#allCategoriesList").html(listingHTML); 
-
-
-    
-    
-//get TOP CATEGORIES
-//loop for categories that I cannot find
-    
-//$("#topCats").text();
-//Mustache.render( , );
-//send to html doc
-
-    
-    
     
 //get ALL CATEGORIES WHISKEY -- loop ALL categories!
-    var allCategories = data.forEach(function(all){
-        return {
-            category_path: all.category_path,  
-        }
-    })
+    var allCatUpdates = {};
+    
+    allCatUpdates = data.results.map(function(obj) {
+        return {listingId: obj.listing_id,
+               url_75x75: obj.Images[0].url_75x75,
+                price: obj.price};
+    });
+    
+    var catUpdates = {
+        allCatUpdates : allCatUpdates
+    };
     
 //send text, render to html, send data via jquery
-//$("#allCats").text();
-//Mustache.render( , );
-$("allCategoriesWhiskey").html()
+var allCatTemplate = $("#allCatTemplate").text();
+var allCatHTML = Mustache.render(allCatTemplate, catUpdates);
+$("#allCatWhiskey").html(allCatHTML);
+    console.log(allCatHTML);
     
     
 });//closes doc ready
